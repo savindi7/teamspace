f414d697448e8315a109991d4d5bd2873a3476fc
savindi7 <savindi@wso2.com>
2025-03-06 14:26:00 +0530

    Update browser autofill styles in all steps

--- step-4/src/theme.ts
+++ step-4/src/theme.ts
@@ -14,6 +14,20 @@ const theme = createTheme({
             main: '#D6A829',
         },
     },
+    components: {
+        MuiInputBase: {
+            styleOverrides: {
+                root: {
+                    "& input:-webkit-autofill": {
+                      boxShadow: "0 0 0px 1000px white inset",
+                      WebkitTextFillColor: "#000",
+                      transition: "background-color 5000s ease-in-out 0s",
+                      caretColor: "#000"
+                    },
+                },
+            },
+        },
+    }
 });
 
 export default theme;
